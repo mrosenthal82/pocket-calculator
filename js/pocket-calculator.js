621ed5a8e835cf630d8e001ac9a7766058a4e80f
Meredith Rosenthal <mrosenthal@ucvts.org>
2019-01-03 13:09:34 -0500

    new input functions

--- js/pocket-calculator.js
+++ js/pocket-calculator.js
@@ -1,129 +1,57 @@
-let val;
-let digitCount=0;
+
+let val = 0;
+let valOne;
+let valTwo;
 let pointCount=0;
 let operation;
+let newDigit;
 
 function setup(){
   let display = document.getElementById("display");
-  val = "0";
-  digitCount=0;
+  val = 0;
   pointCount=0;
   operation="";
   display.innerHTML = val;
 }
 
-function one() {
-  let display = document.getElementById("display");
-  if (digitCount==0){
-    val="1";
-  } else {
-    val +="1";
-  }
-  display.innerHTML = val;
-  digitCount++;
+function one(){
+  combinedValue(1);
+}
+function two(){
+  combinedValue(2);
+}
+function three(){
+  combinedValue(3);
+}
+function four(){
+  combinedValue(4);
+}
+function five(){
+  combinedValue(5);
+}
+function six(){
+  combinedValue(6);
+}
+function seven(){
+  combinedValue(7);
+}
+function eight(){
+  combinedValue(8);
+}
+function nine(){
+  combinedValue(9);
+}
+function zero(){
+  combinedValue(0);
 }
 
-function two() {
+function combinedValue(newDigit) {
   let display = document.getElementById("display");
-  if (digitCount==0){
-    val="2";
-  } else {
-    val +="2";
-  }
+  val=val*10+newDigit;
   display.innerHTML = val;
-  digitCount++;
-}
-
-function three() {
-  let display = document.getElementById("display");
-  if (digitCount==0){
-    val="3";
-  } else {
-    val+="3";
-  }
-  display.innerHTML = val;
-  digitCount++;
-}
-
-function four() {
-  let display = document.getElementById("display");
-  if (digitCount==0){
-    val="4";
-  } else {
-    val +="4";
-  }
-  display.innerHTML = val;
-  digitCount++;
-}
-
-function five() {
-  let display = document.getElementById("display");
-  if (digitCount==0){
-    val="5";
-  } else {
-    val +="5";
-  }
-  display.innerHTML = val;
-  digitCount++;
-}
-
-function six() {
-  let display = document.getElementById("display");
-  if (digitCount==0){
-    val="6";
-  } else {
-    val +="6";
-  }
-  display.innerHTML = val;
-  digitCount++;
-}
-
-function seven() {
-  let display = document.getElementById("display");
-  if (digitCount==0){
-    val="7";
-  } else {
-    val +="7";
-  }
-  display.innerHTML = val;
-  digitCount++;
-}
-
-function eight() {
-  let display = document.getElementById("display");
-  if (digitCount==0){
-    val="8";
-  } else {
-    val +="8";
-  }
-  display.innerHTML = val;
-  digitCount++;
-}
-
-function nine() {
-  let display = document.getElementById("display");
-  if (digitCount==0){
-    val="9";
-  } else {
-    val +="9";
-  }
-  display.innerHTML = val;
-  digitCount++;
-}
-
-function zero() {
-  let display = document.getElementById("display");
-  if (digitCount==0){
-    val="0";
-  } else {
-    val +="0";
-  }
-  display.innerHTML = val;
-  digitCount++;
 }
 
 function point() {
-  let display = document.getElementById("display");
   if (pointCount==0){
     if (digitCount==0){
       val=".";
@@ -139,17 +67,27 @@ function point() {
 }
 
 function add(){
-  let valOne = Number(val);
-  let operation = "add";
-  val = "";
-  display.innerHTML="+";
+  valOne = val;
+  operation = "+";
+  val = 0;
+  display.innerHTML=operation;
 }
 
 function equals() {
-  let valTwo = Number(val);
+  valTwo = val;
   let final;
-  if (operation == add){
+  if (operation == '+'){
     final = valOne + valTwo;
   }
+  if (operation == 'subtract'){
+    final = valOne - valTwo;
+  }
+  if (operation == 'multiply'){
+    final = valOne * valTwo;
+  }
+  if (operation == 'divide'){
+    final = valOne / valTwo;
+  }
   display.innerHTML = final;
+  val = final;
 }
