723f868b55257154dec6ab9c075a68f60736d990
Meredith Rosenthal <mrosenthal@ucvts.org>
2019-01-17 12:30:53 -0500

    reordering functions for clarity

--- js/pocket-calculator.js
+++ js/pocket-calculator.js
@@ -6,46 +6,12 @@ let operation;
 let notDecimal = true;
 let decimalDigits=0;
 let keepVal=false;
+let prevKeyType = ""; /*d for digit, o for operation, e for equals*/
 
 function setup(){
   operation="";
   resetVal();
   show();
-}
-
-function show(){
-  let display = document.getElementById("display");
-  display.innerHTML = val;
-  let valString = val.toString();
-
-  //commas
-    var valArray = [];
-    var valCopy = val;
-    for (let i = 0; i<valString.length; i++){
-      let expo = valString.length-i;
-      valArray[i] = Math.floor(valCopy*10/Math.pow(10, expo));
-      valCopy = Math.floor(valCopy % Math.pow(10, expo-1));
-      // valCopy -= valArray[i]*Math.pow(10,expo)*10;
-    }
-    if (valString.length>=4 && valString.length<7){
-      valArray.splice(valString.length-3, 0, ",");
-    } else if (valString.length>=7){
-      valArray.splice(valString.length-3, 0, ",");
-      valArray.splice(valString.length-6, 0, ",");
-    }
-    display.innerHTML = valArray.join("");
-
-  //scientific notation
-  if (valString.length > 9){
-    display.innerHTML = Number.parseFloat(val).toExponential();
-  }
-
-
-  if (pointCount > 1){
-    display.innerHTML ="ERROR";
-  } else if (operation === '/' && valTwo === 0){
-    display.innerHTML ="ERROR";
-  }
 }
 
 function resetVal(){
@@ -55,10 +21,59 @@ function resetVal(){
   notDecimal=true;
 }
 
-function combinedValue(newDigit) {
-  // if (!keepVal){
-  //   resetVal();
+function point(){
+  if (pointCount==0){
+    notDecimal=false;
+    show();
+    display.innerHTML+=".0";
+  }
+  pointCount++;
+}
+
+function negative(){
+  val=0-val;
+  show();
+}
+
+function percent(){
+  val=val/100;
+  show();
+}
+
+function show(){
+  let display = document.getElementById("display");
+  display.innerHTML = val;
+  let valString = val.toString();
+
+  //commas
+  // var valArray = [];
+  // var valCopy = val;
+  // for (let i = 0; i<valString.length; i++){
+  //   let expo = valString.length-i;
+  //   valArray[i] = Math.floor(valCopy*10/Math.pow(10, expo));
+  //   valCopy = Math.floor(valCopy % Math.pow(10, expo-1));
   // }
+  // if (valString.length>=4 && valString.length<7){
+  //   valArray.splice(valString.length-3, 0, ",");
+  // } else if (valString.length>=7){
+  //   valArray.splice(valString.length-3, 0, ",");
+  //   valArray.splice(valString.length-6, 0, ",");
+  // }
+  // display.innerHTML = valArray.join("");
+
+  //scientific notation
+  if (valString.length > 9){
+    display.innerHTML = Number.parseFloat(val).toExponential();
+  }
+
+  if (pointCount > 1){
+    display.innerHTML ="ERROR";
+  } else if (operation === '/' && valTwo === 0){
+    display.innerHTML ="ERROR";
+  }
+}
+
+function combinedValue(newDigit) {
   if (notDecimal){
     val=val*10+newDigit;
   } else {
@@ -70,15 +85,7 @@ function combinedValue(newDigit) {
     val = Number(val.toFixed(decimalDigits + 1));
   }
   show();
-}
-
-function point() {
-  if (pointCount==0){
-    notDecimal=false;
-    show();
-    display.innerHTML+=".0";
-  }
-  pointCount++;
+  prevKeyType="d";
 }
 
 function useOperation(op){
@@ -86,6 +93,7 @@ function useOperation(op){
   valOne = val;
   resetVal();
   display.innerHTML=op;
+  prevKeyType="o";
 }
 
 function equals() {
@@ -103,16 +111,9 @@ function equals() {
     val = valOne / valTwo;
   }
   show();
-  resetVal();
-}
-
-function negative(){
-  val=0-val;
-  show();
-}
-function percent(){
-  val=val/100;
-  show();
+  if (!keepVal){
+    resetVal();
+  }
 }
 
 // What Do I Need to Fix?
